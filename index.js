caac6d1101dbe5f5d3094999f7b250ea37481c72
Frank Huff <fs-j@live.com>
2019-04-19 14:22:26 -0700

    electron, ipc, ffmpeg

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
+let mainWindow;
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('videoLength', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        mainWindow.webContents.send('videoMeta', metadata.format.duration);
+    });
+});
